31071f894039a66bf52c3509fd65a37886763d55
Diego Gean da Fé <diegogeandafe@gmail.com>
2022-09-07 13:14:31 -0300

    Merge

---
+++ app/src/main/restaurantes/models/Restaurante.js
@@ -0,0 +1,28 @@
+const restaurantes = require('../../data/restaurantes.json');
+
+class Restaurante {
+
+    static getAll() {
+        return new Promise(function(resolve, reject) {
+            resolve(restaurantes);           
+        });
+    }
+
+    static getById(id) {
+        return new Promise(async function(resolve, reject) {
+            const idSalvo = await restaurantes.findIndex((elemento) => {
+                console.log(elemento.id.toString() == id);
+                return elemento.id.toString() == id;
+            });
+
+            if (idSalvo >= 0) {
+                resolve(restaurantes[idSalvo]);
+            }
+            
+            reject('Data not found');
+        });
+    }
+
+}
+
+module.exports = Restaurante;
